1379d4a5b2e2805c46ff2961ecce88440e22acd4
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2023-11-06 21:35:50 +0000

    Updated files for Calculator - Git working fine with IntelliJ

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,17 @@
 public class Calculator {
+    public int add(int x, int y){
+        return x+y;
+    }
 
+    public int subtract(int x, int y){
+        return x-y;
+    }
+
+    public int multiply(int x, int y){
+        return x*y;
+    }
+
+    public double divide(double x, int y){
+        return x/y;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        Calculator sample = new Calculator();
+
+        System.out.println(sample.add(2,5));
+        System.out.println(sample.subtract(2,5));
+        System.out.println(sample.multiply(2,5));
+        System.out.println(sample.divide(2,5));
+
+    }
 }
